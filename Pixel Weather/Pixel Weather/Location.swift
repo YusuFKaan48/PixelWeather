d97a78dc22d693a91f53763238635a20456687e6
Yusuf Kaan USTA <yusufkaanusta@192.168.1.41>
2023-07-10 17:46:41 +0300

    Add: Welcome View

---
+++ Pixel Weather/Pixel Weather/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  Pixel Weather
+//
+//  Created by Yusuf Kaan USTA on 10.07.2023.
+//
+
+import Foundation
---
+++ Pixel Weather/Pixel Weather/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pixel Weather
+//
+//  Created by Yusuf Kaan USTA on 10.07.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
